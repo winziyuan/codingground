4f9d1ce26f12cbd5785dbcac4b939e925fb472c4
xxc <winziyuan@163.com>
2016-06-08 15:55:56 +0800

    Update main.cpp

--- New Project-20160608/main.cpp
+++ New Project-20160608/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include <ctime>
+#include <cstring>
+
 using namespace std;
+
+struct Books
+{
+   char  title[50];
+   char  author[50];
+   char  subject[100];
+   int   book_id;
+};
+
+typedef struct
+{
+   char  title[50];
+   char  author[50];
+   char  subject[100];
+   int   book_id;
+}Book;
 
 int main()
 {
@@ -29,6 +47,46 @@ int main()
    cout << "Time: "<< 1 + ltm->tm_hour << ":";
    cout << 1 + ltm->tm_min << ":";
    cout << 1 + ltm->tm_sec << endl;
+   //test2
+   char name[50];
+   cout << "请输入您的名称： ";
+   //cin >> name;
+   cout << "您的名称是： " << name << endl;
+   char str[] = "Unable to read....";
+   cerr << "Error message : " << str << endl;
+   char str2[] = "Unable to read....";
+   clog << "Error message : " << str2 << endl;
+   
+   
+   //test3
+   struct Books Book1;        // 声明 Book1，类型为 Book
+   struct Books Book2;        // 声明 Book2，类型为 Book
+   // Book1 详述
+   strcpy( Book1.title, "Learn C++ Programming");
+   strcpy( Book1.author, "Chand Miyan"); 
+   strcpy( Book1.subject, "C++ Programming");
+   Book1.book_id = 6495407;
+
+   // Book2 详述
+   strcpy( Book2.title, "Telecom Billing");
+   strcpy( Book2.author, "Yakit Singha");
+   strcpy( Book2.subject, "Telecom");
+   Book2.book_id = 6495700;
+ 
+   // 输出 Book1 信息
+   cout << "Book 1 title : " << Book1.title <<endl;
+   cout << "Book 1 author : " << Book1.author <<endl;
+   cout << "Book 1 subject : " << Book1.subject <<endl;
+   cout << "Book 1 id : " << Book1.book_id <<endl;
+
+   // 输出 Book2 信息
+   cout << "Book 2 title : " << Book2.title <<endl;
+   cout << "Book 2 author : " << Book2.author <<endl;
+   cout << "Book 2 subject : " << Book2.subject <<endl;
+   cout << "Book 2 id : " << Book2.book_id <<endl;
+  
+   //test4
+   Book book,book2;   
    return 0;
 }
 
